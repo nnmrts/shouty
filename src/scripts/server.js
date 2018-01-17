2992730e1beead3352e592e4405ad8bc13800577
Nano Miratus <nanomiratus@gmail.com>
2018-01-17 20:39:19 +0100

    moved server.js

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const shouty = require("./shouty.js");
 
 const app = express();
 
 app.use(express.static("./"));
 
 app.get("/", (req, res) => {
+	console.log(req);
+	shouty.init();
 	res.sendFile(`${__dirname}index.html`);
 });
 
