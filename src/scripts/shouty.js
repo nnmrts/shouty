fe6071df937666d5fb99f4d5ad74ee648d772f98
Nano Miratus <nanomiratus@gmail.com>
2018-01-17 20:42:34 +0100

    wrapped angular app declaration inside an init function

--- src/scripts/shouty.js
+++ src/scripts/shouty.js
@@ -6,53 +6,57 @@ const Controllers = {
 	LoginCtrl
 };
 
+const shouty = {
+	init() {
+		/**
+		 * @name shoutyApp
+		 * @description
+		 * # shoutyApp
+		 *
+		 * Main module of the application.
+		 */
+		const shoutyApp = angular
+			.module("shoutyApp", [
+				"ngAnimate",
+				"ngAria",
+				"ngCookies",
+				"ngMessages",
+				"ngResource",
+				"ngRoute",
+				"ngSanitize",
+				"ngTouch",
+				"ui.router"
+			])
+			.config(($locationProvider, $stateProvider, $urlRouterProvider) => {
+				const startState = {
+					name: "start",
+					url: "/",
+					templateUrl: "./views/start.html",
+					controller: "StartCtrl"
+				};
+				const loginState = {
+					name: "login",
+					url: "/login",
+					templateUrl: "./views/login.html",
+					controller: "LoginCtrl"
+				};
 
-/**
- * @name shoutyApp
- * @description
- * # shoutyApp
- *
- * Main module of the application.
- */
-const shouty = angular
-	.module("shoutyApp", [
-		"ngAnimate",
-		"ngAria",
-		"ngCookies",
-		"ngMessages",
-		"ngResource",
-		"ngRoute",
-		"ngSanitize",
-		"ngTouch",
-		"ui.router"
-	])
-	.config(($locationProvider, $stateProvider, $urlRouterProvider) => {
-		const startState = {
-			name: "start",
-			url: "/",
-			templateUrl: "./views/start.html",
-			controller: "StartCtrl"
-		};
-		const loginState = {
-			name: "login",
-			url: "/login",
-			templateUrl: "./views/login.html",
-			controller: "LoginCtrl"
-		};
+				$stateProvider.state(startState);
+				$stateProvider.state(loginState);
 
-		$stateProvider.state(startState);
-		$stateProvider.state(loginState);
+				$urlRouterProvider.otherwise("/");
 
-		$urlRouterProvider.otherwise("/");
+				$locationProvider.html5Mode({
+					enabled: true,
+					requireBase: false
+				});
+			});
 
-		$locationProvider.html5Mode({
-			enabled: true,
-			requireBase: false
+		Object.keys(Controllers).forEach((name) => {
+			shoutyApp.controller(name, Controllers[name]);
 		});
-	});
+	}
+};
 
-Object.keys(Controllers).forEach((name) => {
-	shouty.controller(name, Controllers[name]);
-});
 
 export default shouty;
