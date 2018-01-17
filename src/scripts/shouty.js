7e5133f55a3e9e6e975f6bf720f92319eb7ee7ee
Nano Miratus <nanomiratus@gmail.com>
2018-01-17 22:14:28 +0100

    added controller for nav

--- src/scripts/shouty.js
+++ src/scripts/shouty.js
@@ -1,9 +1,11 @@
 import StartCtrl from "./controllers/start.js";
 import LoginCtrl from "./controllers/login.js";
+import NavCtrl from "./controllers/nav.js";
 
 const Controllers = {
 	StartCtrl,
-	LoginCtrl
+	LoginCtrl,
+	NavCtrl
 };
 
 /**
