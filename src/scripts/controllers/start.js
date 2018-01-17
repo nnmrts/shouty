a1d107acbb523c29d13b46fd55aeb7403aafa58f
Nano Miratus <nanomiratus@gmail.com>
2018-01-17 22:42:11 +0100

    added request to messages database

--- src/scripts/controllers/start.js
+++ src/scripts/controllers/start.js
@@ -3,10 +3,15 @@
  * @name StartCtrl
  * @description
  * @param {any} $scope angular scope
+ * @param {any} $http angular $http service
  * @ngInject
  */
-const StartCtrl = function($scope) {
+const StartCtrl = function($scope, $http) {
 	$scope.name = "start";
+
+	$http.get("./messages.json").then((response) => {
+		console.log(response);
+	});
 
 	window[`${$scope.name}Scope`] = $scope;
 };
