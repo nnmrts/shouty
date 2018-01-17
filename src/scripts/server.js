a70091363b1e35573287aba70c1800467ee46405
Nano Miratus <nanomiratus@gmail.com>
2018-01-17 21:14:28 +0100

    removed shouty from server tasks

--- src/scripts/server.js
+++ src/scripts/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const shouty = require("./shouty.js");
 
 const app = express();
 
 app.use(express.static("./"));
 
-app.get("/", (req, res) => {
-	console.log(req);
-	shouty.init();
-	res.sendFile(`${__dirname}index.html`);
+app.post("send", (req, res) => {
+	console.log(res);
+	console.log("SENT MESSAGE");
 });
 
 app.listen(9000, () => {
