7b5b368aad74975f044b87607f5ca6d5eaffa7b3
Nano Miratus <nanomiratus@gmail.com>
2018-01-17 19:43:11 +0100

    removed unexpected y

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const openAdb = require("./open-adb.js");
 
 const app = express();
 
-app.use(express.static("./"));y
+app.use(express.static("./"));
 
 app.get("/", (req, res) => {
 	res.sendFile(`${__dirname}index.html`);
