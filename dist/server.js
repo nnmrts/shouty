3a091502c3d2abcfd3e4cf193cdaadd1e52d59cb
Nano Miratus <nanomiratus@gmail.com>
2018-01-18 20:21:58 +0100

    Build: generated dist files
    Signed-off-by: Nano Miratus <nanomiratus@gmail.com>

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,43 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const jsonfile = require("jsonfile");
+const useragent = require("express-useragent");
 
 const app = express();
 
 app.use(express.static("./"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
-app.post("send", (req, res) => {
-	console.log(res);
-	console.log("SENT MESSAGE");
+app.enable("trust proxy");
+
+app.post("/", (req, res) => {
+	jsonfile.readFile("./messages.json", (error, messages) => {
+		const body = {
+			username: req.body.username,
+			message: req.body.message,
+			ip: req.ip,
+			useragent: {
+				browser: {
+					name: useragent.parse(req.headers["user-agent"]).browser,
+					version: useragent.parse(req.headers["user-agent"]).version
+				},
+				platform: {
+					name: useragent.parse(req.headers["user-agent"]).platform,
+					os: useragent.parse(req.headers["user-agent"]).os
+				},
+				source: useragent.parse(req.headers["user-agent"]).source
+			}
+		};
+
+		messages.push(body);
+
+		jsonfile.writeFile("./messages.json", messages, () => {
+			res.send("success");
+		});
+	});
 });
 
 app.listen(9000, () => {
