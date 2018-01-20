f235dc90cdeb17a881e72b8283cc5980c48d249f
Nano Miratus <nanomiratus@gmail.com>
2018-01-20 14:50:57 +0100

    added sse and fixed image saving

--- server.js
+++ server.js
@@ -2,21 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jsonfile = require("jsonfile");
 const useragent = require("express-useragent");
-const path = require("path");
 const multer = require("multer");
+const fs = require("fs");
+const sse = require("./sse.js");
 
 const app = express();
-
-const storage = multer.diskStorage({
-	destination: "./uploads/",
-	filename(req, file, cb) {
-		cb(null, `${file.originalname.replace(path.extname(file.originalname), "")}-${Date.now()}${path.extname(file.originalname)}`);
-	}
-});
-
-const upload = multer({
-	storage
-});
+const connections = [];
 
 app.set("port", (process.env.PORT || 9000));
 
@@ -25,44 +16,70 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+app.use(sse);
 
 app.enable("trust proxy");
 
-app.post("/", (req, res) => {
-	jsonfile.readFile("./dist/messages.json", (error, messages) => {
-		const newMessages = messages;
+jsonfile.readFile("./dist/messages.json", (error, messages) => {
+	const newMessages = messages;
 
-		newMessages.push(Object.assign(() => {
-			delete req.body.chatTime;
+	app.post("/", (req, res) => {
+		const message = req.body;
 
-			return req.body;
-		}, {
-			ip: req.ip,
-			useragent: {
-				browser: {
-					name: useragent.parse(req.headers["user-agent"]).browser,
-					version: useragent.parse(req.headers["user-agent"]).version
-				},
-				platform: {
-					name: useragent.parse(req.headers["user-agent"]).platform,
-					os: useragent.parse(req.headers["user-agent"]).os
-				},
-				source: useragent.parse(req.headers["user-agent"]).source
+		console.log(message);
+
+		fs.writeFile(`./dist/images/${message.image.name}`, message.image.file, "base64", (err) => {
+			if (err) {
+				return console.log(err);
 			}
-		}));
 
-		while (newMessages.length > 8) {
-			newMessages.shift();
-		}
+			console.log("The file was saved!");
 
-		jsonfile.writeFile("./dist/messages.json", newMessages, () => {
-			res.send("success");
+
+			newMessages.push(Object.assign((() => {
+				delete message.chatTime;
+				message.image = message.image.name;
+
+				return message;
+			})(), {
+				ip: req.ip,
+				useragent: {
+					browser: {
+						name: useragent.parse(req.headers["user-agent"]).browser,
+						version: useragent.parse(req.headers["user-agent"]).version
+					},
+					platform: {
+						name: useragent.parse(req.headers["user-agent"]).platform,
+						os: useragent.parse(req.headers["user-agent"]).os
+					},
+					source: useragent.parse(req.headers["user-agent"]).source
+				}
+			}));
+
+			while (newMessages.length > 8) {
+				newMessages.shift();
+			}
+
+			return jsonfile.writeFile("./dist/messages.json", newMessages, (innerError) => {
+				if (innerError) {
+					return console.log(innerError);
+				}
+				connections.forEach((connection) => {
+					connection.sseSend(newMessages);
+				});
+
+				res.sendStatus(200);
+
+				return res.send("success");
+			});
 		});
 	});
-});
 
-app.post("/image", upload.single("file"), (req) => {
-	console.log("Uploade Successful ", req.file, req.body);
+	app.get("/stream", (req, res) => {
+		res.sseSetup();
+		res.sseSend(newMessages);
+		connections.push(res);
+	});
 });
 
 app.listen(app.get("port"), () => {
---
+++ sse.js
@@ -0,0 +1,24 @@
+/**
+ * @name sse
+ *
+ * @param {any} req middleware req object
+ * @param {any} res middleware res object
+ * @param {any} next middleware next function
+ */
+const sse = (req, res, next) => {
+	res.sseSetup = () => {
+		res.writeHead(200, {
+			"Content-Type": "text/event-stream",
+			"Cache-Control": "no-cache",
+			Connection: "keep-alive"
+		});
+	};
+
+	res.sseSend = function(data) {
+		res.write(`data: ${JSON.stringify(data)}\n\n`);
+	};
+
+	next();
+};
+
+module.exports = sse;
