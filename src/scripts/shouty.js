c74ed082f13fd0e030346b4d06f3fc6436d9ea31
Nano Miratus <nanomiratus@gmail.com>
2018-01-23 03:58:35 +0100

    added viewport bug fix

--- src/scripts/shouty.js
+++ src/scripts/shouty.js
@@ -1,3 +1,5 @@
+import $ from "jquery";
+
 import map from "./map.js";
 import ShoutyCfg from "./cfg.js";
 
@@ -25,6 +27,13 @@ Object.keys(map).forEach((key) => {
 		ShoutyApp[key.slice(0, -1)](name, map[key][name]);
 	});
 });
+
+setTimeout(() => {
+	const viewheight = $(window).height();
+	const viewwidth = $(window).width();
+	const viewport = document.querySelector("meta[name=viewport]");
+	viewport.setAttribute("content", `height=${viewheight}px, width=${viewwidth}px, initial-scale=1.0`);
+}, 300);
 
 
 export default ShoutyApp;
